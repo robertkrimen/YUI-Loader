df6131c383fa0320716108dd210738910dfad152
robertkrimen <robertkrimen@gmail.com>
2009-02-21 12:40:24 -0800

    Converting from JS::YUI::Loader to YUI::Loader: moved files

--- lib/JS/YUI/Loader/Carp.pm
+++
@@ -1,5 +0,0 @@
-package JS::YUI::Loader::Carp;
-
-use Carp::Clan::Share;
-
-1;
--- lib/JS/YUI/Loader/Source/YUIHost.pm
+++
@@ -1,6 +0,0 @@
-package JS::YUI::Loader::Source::YUIHost;
-
-use Moose;
-extends qw/JS::YUI::Loader::Source::Internet/;
-
-1;
